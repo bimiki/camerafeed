9646583fd47faacacf4ea973c4d771c820591dc5
BiMiKi <noahtamulonis@gmail.com>
2021-12-18 16:18:22 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :videofeeds
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "videofeeds#index"
