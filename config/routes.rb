569edc59528cf683369cea38f2e0d61df6e60899
BiMiKi <noahtamulonis@gmail.com>
2021-12-19 11:24:25 -0500

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
 resources :videofeeds
-  devise_for :users
+
+  devise_for :users #should this be first?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "videofeeds#index"
-  
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
